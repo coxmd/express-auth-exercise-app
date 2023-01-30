51a95e404adaa105c8c9db8f3b763c7c24aa1af6
coxmd <coxmusyoki@gmail.com>
2023-01-30 15:39:38 +0300

    Completed configuring the app.js file

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 require('dotenv').config();
 
+const createError = require('http-errors')
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-// const SQliteStore = require('connect-sqlite3')(session);
+const SQliteStore = require('connect-sqlite3')(session);
 
 const app = express();
 
 app.locals.pluralize = require('pluralize');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// a session secret is used to compute hash
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQliteStore({db: 'sessionsStorage.db', dir: './var/dir'})
+}));
+
+app.use(passport.authenticate('session'));
+app.use(function(req, res, next) {
+    const msgs = req.session.messages || [];
+    res.locals.messages = msgs;
+    res.locals.hasMessages = !! msgs.length;
+    req.session.messages = [];
+    next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+})
+
+// error handler
+app.use(function(err, req, res) {
+    // set locals, only providing error in dev
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render error page
+    res.status(err.status || 500);
+    res.render('error');
+})
 
 module.exports = app;
