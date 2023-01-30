d96064257c7808cab1b742e26d0c86f4e470cf30
coxmd <coxmusyoki@gmail.com>
2023-01-30 11:38:02 +0300

    Completed setting up the server by setting up a port

--- bin/www.js
+++ bin/www.js
@@ -2,5 +2,22 @@
 We want to separate our app logic from actually running the app
 in production - you would want to use something else that is more
 robust such as unitech
-
 */
+
+const app = require('../app');
+const http = require('http');
+
+// get port from environment and store it in epress
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+
+// create an HTTP server
+
+const server = http.createServer(app);
+
+// listen on provide port, on all network interface
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
