2c0cfec235c9fddb50ea414fc675af24537acc07
coxmd <coxmusyoki@gmail.com>
2023-01-30 11:15:00 +0300

    Implemented setting up the application server

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const SQliteStore = require('connect-sqlite3')(session);
+
+const app = express();
+
+app.locals.pluralize = require('pluralize');
+
+modules.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,6 @@
+/* 
+We want to separate our app logic from actually running the app
+in production - you would want to use something else that is more
+robust such as unitech
+
+*/
